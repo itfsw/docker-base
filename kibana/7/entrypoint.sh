da2aa58c7d4018fd724b52c63440a03e52271d1b
hewei <624371359@qq.com>
2021-11-17 15:13:16 +0800

    kibana 7

--- kibana/7/entrypoint.sh
+++ kibana/7/entrypoint.sh
@@ -3,6 +3,5 @@
 # 读取环境变量写入htpasswd文件
 echo "$AUTH_HTPASSWD" > /etc/nginx/conf.d/htpasswd
 
-# 启动kibana和Nginx，注意默认用户已经切换为root,这样才能执行写入htpasswd
-# 但是kibana官方镜像里建议启动用户为kibana，需要注意处理
-systemctl start nginx && su -u kibana -c /docker-entrypoint.sh kibana
+# 启动kibana和Nginx
+/usr/sbin/nginx && /docker-entrypoint.sh kibana
